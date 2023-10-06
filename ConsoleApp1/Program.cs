db85a59474fca4894013686fa1c52790ad6ba80d
Lakhdar Khl <82035622+LakhdarKhl@users.noreply.github.com>
2023-10-06 14:28:40 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Entrez toutes vos factures");
+
+
+
+
+string []answer = new string[12];
+
+for(int i = 0; i < answer.Length; i++)
+{
+    answer[i] = Console.ReadLine();
+}
+
+int = Convert.ToInt32();
+
+Console.WriteLine("Finito , appuie sur autre chose sinn jte tue");
+Console.ReadLine();
